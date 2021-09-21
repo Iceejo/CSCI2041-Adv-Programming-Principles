f9d48864fcd241033b993c8c552690972d8fb356
Kim Ngo <ngo00046@umn.edu>
2021-09-21 09:22:39 -0500

    Completing Lab 3

---
+++ Lab_03/lab_03.ml
@@ -0,0 +1,28 @@
+open Char
+open String
+
+let rec sum_of_floats xs = 
+  match xs with
+  | [] -> 0.
+  |x::rest -> x +. sum_of_floats rest
+
+let rec square_floats xs = 
+  match xs with
+  |[]->[]
+  |x::rest -> x*.x::square_floats rest
+
+let rec exclaimify xs = 
+  match xs with
+  | [] -> []
+  | s::rest -> (s ^ "!") :: exclaimify rest
+
+let rec remove_odds xs = 
+  match xs with
+  | [] -> []
+  | x::rest when x mod 2 = 0 -> x::remove_odds rest
+  | x::rest -> remove_odds rest
+
+let rec remove_caps xs = 
+  match xs with
+  | [] -> []
+  | x::rest -> if Char.equal (String.get x 0) (Char.uppercase_ascii (String.get x 0)) then remove_caps rest else x::remove_caps rest
